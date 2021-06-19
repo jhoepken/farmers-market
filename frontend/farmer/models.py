40db98d18119cd22a5ed27b844c6185abb33e2b2
Jens Höpken <jhoepken@gmail.com>
2021-06-19 14:13:03 +0200

    Add Farmer model

--- frontend/farmer/models.py
+++ frontend/farmer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Farmer(models.Model):
+    name = models.CharField(max_length=200)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=25)
+    zip_code = models.IntegerField()
